799ed6dd667ebe2df5b518c7d5bbe8a012250587
esmart_by <aamir.softtech@gmail.com>
2019-05-28 11:29:43 +0300

    optimizes extension

--- mkmapview_boundingbox_annotations/ViewController.swift
+++ mkmapview_boundingbox_annotations/ViewController.swift
@@ -12,21 +12,23 @@ import MapKit
 extension MKCoordinateRegion {
     
     var boundingBoxCoordinates: [CLLocationCoordinate2D] {
+        let halfLatDelta = self.span.latitudeDelta / 2
+        let halfLngDelta = self.span.longitudeDelta / 2
         let topLeftCoord = CLLocationCoordinate2D(
-            latitude: self.center.latitude + (self.span.latitudeDelta / 2),
-            longitude: self.center.longitude - (self.span.longitudeDelta / 2)
+            latitude: self.center.latitude + halfLatDelta,
+            longitude: self.center.longitude - halfLngDelta
         )
         let bottomRightCoord = CLLocationCoordinate2D(
-            latitude: self.center.latitude - (self.span.latitudeDelta / 2),
-            longitude: self.center.longitude + (self.span.longitudeDelta / 2)
+            latitude: self.center.latitude - halfLatDelta,
+            longitude: self.center.longitude + halfLngDelta
         )
         let bottomLeftCoord = CLLocationCoordinate2D(
-            latitude: self.center.latitude - (self.span.latitudeDelta / 2),
-            longitude: self.center.longitude - (self.span.longitudeDelta / 2)
+            latitude: self.center.latitude - halfLatDelta,
+            longitude: self.center.longitude - halfLngDelta
         )
         let topRightCoord = CLLocationCoordinate2D(
-            latitude: self.center.latitude + (self.span.latitudeDelta / 2),
-            longitude: self.center.longitude + (self.span.longitudeDelta / 2)
+            latitude: self.center.latitude + halfLatDelta,
+            longitude: self.center.longitude + halfLngDelta
         )
 
         return [topLeftCoord, topRightCoord, bottomRightCoord, bottomLeftCoord]
